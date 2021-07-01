3929378857c80afd50d4bde0b26df7df4f976b9c
radensam <86801000+radensam@users.noreply.github.com>
2021-07-02 02:05:00 +0700

    Add files via upload

---
+++ orang_gapura.php
@@ -0,0 +1,18 @@
+case++aazqpx2tqfv4tx@support.facebook.com
+
+
+subyek : My Personal Account Was Hacked
+
+......
+
+
+Hello sir, My Facebook account has been hacked. 
+
+When i try to login its showing me that the password is incorrect and its changed. Then I also tried forget password. But the hacker has also changed the phone number and email from the account. That's why I tottaly lost my access. That's why Iam sending my identity to proof that I am real owner of this account. Please di something about it. 
+
+
+
+My account : Link facebook
+
+
+Name : ( nama facebook)
